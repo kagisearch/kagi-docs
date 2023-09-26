838de95f4512ff6daf694ab4e7d781be1128074b
Luan <8280546+mozedz@users.noreply.github.com>
2023-09-26 17:04:40 +0700

    update esbuild version

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -61,7 +61,7 @@ export default defineConfig({
             text: 'Edit this page on GitHub'
         }
     },
-    rewrites: {'index.md': 'kagi/index.md',},
+    // rewrites: {'index.md': 'kagi/index.md',},
     vite: {
         resolve: {
             preserveSymlinks: true,
