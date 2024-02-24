40613c087da1efff4b90c50b251a8d4f7f201ac0
david-ros <138538459+david-ros@users.noreply.github.com>
2024-02-23 18:42:32 -0600

    Update config.ts (#358)
    Add link to Kagi Sidekick page to the Sidebar

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -262,7 +262,8 @@ function sidebarKagi() {
                         { text: 'Search API', link: '/kagi/api/search' },
                         { text: 'Enrichment API', link: '/kagi/api/enrich' },
                         { text: 'Small Web RSS Feed', link: '/kagi/api/smallweb' },
-                    ]
+                    ],
+                    text: 'Kagi Sidekick', link: '/kagi/sidekick/',
                 }
 
             ]
