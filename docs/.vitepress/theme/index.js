12a12801f39d4ab2a046ec14a0f811d32057cc13
Anton Ždanov <anton@azdanov.dev>
2024-12-22 13:54:07 +0200

    cleanup

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,5 @@
 import DefaultTheme from "vitepress/theme";
 import queryHack from "../custom_scripts/search_query_hack";
-import { handleSort } from "../custom_scripts/sort";
 import GenericTable from '../components/GenericTable.vue'
 import { onMounted, watch, nextTick } from 'vue';
 import { useRoute } from 'vitepress';
@@ -11,7 +10,6 @@ export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
     queryHack();
-    handleSort();
     app.component("GenericTable", GenericTable)
   },
   setup() {
