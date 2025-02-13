785bc751755d2ef7b23b39df5f15729b77c1a059
David <david@MacBook-Pro-de-David.local>
2025-02-13 14:01:18 -0600

    Add link to Using Kagi with Tor page in the Sidebar
    Add link to Using Kagi with Tor page in the Sidebar

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -163,6 +163,7 @@ function sidebarKagi() {
                         { text: 'Private Browser Sessions', link: '/kagi/privacy/private-browser-sessions' },
                         { text: 'Two Factor Authentication', link: '/kagi/privacy/two-factor-authentication' },
                         { text: 'Privacy Pass', link: '/kagi/privacy/privacy-pass' },
+                        { text: 'Using Kagi with Tor', link: '/kagi/privacy/tor' },
                         { text: 'Log in with QR Code', link: '/kagi/privacy/log-in-with-qr-code' },
                         { text: 'Content Policy', link: '/kagi/privacy/content-policy' },
                         { text: 'Security', link: '/kagi/privacy/security' },
