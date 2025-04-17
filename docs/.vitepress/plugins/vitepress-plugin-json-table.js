8b048e16cdb3d0e02ee006490102b6dfa26d2036
Xytronix <32957125+Xytronix@users.noreply.github.com>
2025-04-17 23:41:51 +0200

    Create vitepress-plugin-json-table.js

---
+++ docs/.vitepress/plugins/vitepress-plugin-json-table.js
@@ -0,0 +1,44 @@
+import fs from 'fs'
+
+export function vitepressPluginJsonTable() {
+  return {
+    name: 'vitepress-json-table',
+    config() {
+      return {
+        markdown: {
+          config(md) {
+            const defaultFence = md.renderer.rules.fence || ((tokens, idx) => tokens[idx].content)
+
+            md.renderer.rules.fence = (tokens, idx, options, env, slf) => {
+              const token = tokens[idx]
+              const info = token.info.trim()
+
+              if (info === 'json:table') {
+                try {
+                  const json = JSON.parse(token.content)
+                  let props = ''
+
+                  if (json.fields && json.items) {
+                    props += `:fields='${JSON.stringify(json.fields).replace(/'/g, '&#39;')}' `
+                    props += `:items='${JSON.stringify(json.items).replace(/'/g, '&#39;')}' `
+                    if (json.filter) {
+                      props += `:filter='true' `
+                    }
+                  } else {
+                    props = `:data='${JSON.stringify(json).replace(/'/g, '&#39;')}'`
+                  }
+
+                  return `<div class='overflow-x-auto'><SortableTable ${props}/></div>`
+                } catch (e) {
+                  return `<pre>Invalid JSON: ${e.message}</pre>`
+                }
+              }
+
+              return defaultFence(tokens, idx, options, env, slf)
+            }
+          }
+        }
+      }
+    }
+  }
+}
